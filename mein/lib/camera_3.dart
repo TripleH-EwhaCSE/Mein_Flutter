940c61a22b648749710eb8211062f548e78a6453
christina-ohjoo <ohjoo1212@gmail.com>
2021-03-29 20:45:20 +0900

    갤러리추가

--- mein/lib/camera_3.dart
+++ mein/lib/camera_3.dart
@@ -5,35 +5,39 @@ import 'package:photo_manager/photo_manager.dart';
 import 'package:mein/bottomnavigationbar.dart';
 import 'dart:async';
 
-/*
-class Camera_3 extends StatefulWidget {
-  @override
-  _Camera_3_State createState() => _Camera_3_State();
-}
-*/
-//void main() => runApp(MyApp());
-/*
-class Camera_3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-*/
-
 class Camera_3 extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+//사진 찍기
 
 class _MyHomePageState extends State<Camera_3> {
   File _image;
+  PickedFile _image_2;
   final picker = ImagePicker();
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+/*
+  Future getGallery() async {
+    final PickedFile image = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _image_2 = image;
+    });
+  }
+  */
+  Future getGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
 
     setState(() {
       if (pickedFile != null) {
@@ -51,227 +55,46 @@ class _MyHomePageState extends State<Camera_3> {
         title: Text('Select the Image'),
       ),
       body: Center(
+        /*
+        children: <Widget>[
+          Container(
+            child: _image == null
+                ? Text('No image selected.')
+                : Image.file(_image)),
+          Container(
+            child: _image_2 == null
+                ? Text('No image')
+                : Image.file(File(_image_2.path)))
+      ]
+      */
         child: _image == null ? Text('No image selected.') : Image.file(_image),
+        //child: _image_2 == null ? Text('No image'): Image.file(File(_image_2.path)),
+        //_image == null ? Text('No image selected.') : Image.file(_image),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 80),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                heroTag: "btn1",
+                onPressed: getImage,
+                child: Icon(Icons.camera_alt),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              heroTag: "btn2",
+              onPressed: getGallery,
+              child: Icon(Icons.add_photo_alternate),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigation(currentTab: 0),
     );
   }
 }
-
-/*
-      void main() { 
-        runApp(MaterialApp( 
-          home: MaterialApp( 
-            title: '앨범 + 카메라', 
-            home: Scaffold( 
-              appBar: AppBar(title: Text('앨범 + 카메라'),
-              ), 
-              body: Camera_3(), 
-            ) 
-          ) 
-        ));    
-     }
-*/
-
-/*
-Future<void> main() async {
-  // 디바이스에서 이용가능한 카메라 목록을 받아옵니다.
-  final cameras = await availableCameras();
-
-  // 이용가능한 카메라 목록에서 특정 카메라를 얻습니다.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: Camera_3(
-        // 적절한 카메라를 TakePictureScreen 위젯에게 전달합니다.
-        camera: firstCamera,
-      ),
-    ),
-  );
-}
-
-// 사용자가 주어진 카메라를 사용하여 사진을 찍을 수 있는 화면
-class Camera_3 extends StatefulWidget {
-  final CameraDescription camera;
-
-  const Camera_3({
-    Key key,
-    @required this.camera,
-  }) : super(key: key);
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<Camera_3> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    // 카메라의 현재 출력물을 보여주기 위해 CameraController를 생성합니다.
-    _controller = CameraController(
-      // 이용 가능한 카메라 목록에서 특정 카메라를 가져옵니다.
-      widget.camera,
-      // 적용할 해상도를 지정합니다.
-      ResolutionPreset.medium,
-    );
-
-    // 다음으로 controller를 초기화합니다. 초기화 메서드는 Future를 반환합니다.
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // 위젯의 생명주기 종료시 컨트롤러 역시 해제시켜줍니다.
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      // 카메라 프리뷰를 보여주기 전에 컨트롤러 초기화를 기다려야 합니다. 컨트롤러 초기화가
-      // 완료될 때까지 FutureBuilder를 사용하여 로딩 스피너를 보여주세요.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // Future가 완료되면, 프리뷰를 보여줍니다.
-            return CameraPreview(_controller);
-          } else {
-            // 그렇지 않다면, 진행 표시기를 보여줍니다.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        // onPressed 콜백을 제공합니다.
-        onPressed: () async {
-          // try / catch 블럭에서 사진을 촬영합니다. 만약 뭔가 잘못된다면 에러에
-          // 대응할 수 있습니다.
-          try {
-            // 카메라 초기화가 완료됐는지 확인합니다.
-            await _initializeControllerFuture;
-
-            // path 패키지를 사용하여 이미지가 저장될 경로를 지정합니다.
-            final path = join(
-              // 본 예제에서는 임시 디렉토리에 이미지를 저장합니다. `path_provider`
-              // 플러그인을 사용하여 임시 디렉토리를 찾으세요.
-              (await getTemporaryDirectory()).path,
-              '${DateTime.now()}.png',
-            );
-
-            // 사진 촬영을 시도하고 저장되는 경로를 로그로 남깁니다.
-            //await _controller.takePicture(path);
-
-            // 사진을 촬영하면, 새로운 화면으로 넘어갑니다.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imagePath: path),
-              ),
-            );
-          } catch (e) {
-            // 만약 에러가 발생하면, 콘솔에 에러 로그를 남깁니다.
-            print(e);
-          }
-        },
-      ),
-    );
-  }
-}
-
-// 사용자가 촬영한 사진을 보여주는 위젯
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
-      // 이미지는 디바이스에 파일로 저장됩니다. 이미지를 보여주기 위해 주어진
-      // 경로로 `Image.file`을 생성하세요.
-      body: Image.file(File(imagePath)),
-    );
-  }
-}
-*/
-
-/*
-final _picker = ImagePicker();
-
-// ignore: camel_case_types
-class Camera_3 extends StatefulWidget {
-  @override
-  _Camera_3_State createState() => _Camera_3_State();
-}
-
-// ignore: camel_case_types
-class _Camera_3_State extends State<Camera_3> {
-  File _image;
-  final picker = ImagePicker();
-
-  Future<void> _takePicture() async {
-    final imageFile = await ImagePicker.platform
-        .pickImage(source: ImageSource.camera, maxWidth: 60);
-
-    setState(() {
-      _image = File(imageFile.path);
-    });
-  }
-
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Select Image'),
-      ),
-      body: Center(
-        child: _image != null
-            ? Image.file(
-                _image,
-                fit: BoxFit.cover,
-                width: double.infinity,
-              )
-            : Text(
-                'No image selected.',
-                textAlign: TextAlign.center,
-              ),
-        //alignment: Alignment.center,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
-      ),
-      bottomNavigationBar: BottomNavigation(currentTab: 0),
-    );
-  }
-}
-
-*/
